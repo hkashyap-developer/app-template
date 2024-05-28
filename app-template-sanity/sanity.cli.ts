8afc8303e4591f4348f61d4995eb1702c723b965
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-05-28 23:16:39 +0530

    Sanity Implemeted
    Sanity Implemented

---
+++ app-template-sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4ozwfyi6',
+    dataset: 'production'
+  }
+})
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ["cdn.sanity.io", "via.placeholder.com"],
+    },
+  };
+  
+  export default nextConfig;
 
-export default nextConfig;
+
+
+
+
+/*
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: "img.clerk.com",
+        },
+        {
+          hostname: "firebasestorage.googleapis.com",
+        },
+      ],
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  export default nextConfig;
+  */
