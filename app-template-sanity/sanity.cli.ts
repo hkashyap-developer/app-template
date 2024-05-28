3e705fb23d54dc9d19c09de4afd5a438bc21412f
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-05-29 00:10:56 +0530

    test
    test

--- app-template-sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '4ozwfyi6',
-    dataset: 'production'
-  }
-})
--- app-template-sanity/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import {eventType} from './eventType'
-import {artistType} from './artistType'
-import {venueType} from './venueType'
-
-export const schemaTypes = [artistType, eventType, venueType]
